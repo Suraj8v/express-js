1055b50cf23ddc387fba235c64f85f6aefe11b79
suraj8v <jagdalesuraj23@gmail.com>
2024-09-04 17:11:03 +0530

    >

---
+++ HTTP server-2/App2.jsx
@@ -0,0 +1,37 @@
+const express = require('express')
+const app = express()
+const zod = require("zod")
+
+const schema = zod.number()
+
+app.use(express.json())
+
+function userMiddleware(req,res,next)
+{
+    if(req.headers.username==="suraj" && req.headers.password === "123")
+    {
+        next();
+    } else{
+        res.status(403).json({ msg : "Invaid input!"})
+    }
+}
+
+function kidneyMiddleWare (req, res, next)
+{
+    const response = schema.safeParse(req.query.kidneyId)
+    if(req.query.kidneyId==1 || req.query.kidneyId==2)
+    {
+        next()
+    } else{
+        res.status(403).json({ msg : "invalid input!"})
+    }
+}
+
+app.get("/kidney-checkup",userMiddleware,kidneyMiddleWare,function(req,res)
+{
+    res.json({msg : "Your kidney is healthy!"})
+})
+
+app.listen(3000, ()=>{
+    console.log("your app is running at port 3000")
+})
